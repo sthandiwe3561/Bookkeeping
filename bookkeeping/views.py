da272d6c1eb2104d99c8803af6f1c10536b4a431
Sthandiwe Msweli <sthandiwemsweli35@gmail.com>
2025-05-12 11:09:40 +0200

    Add dashboard page: created view, URL pattern, and HTML template

--- bookkeeping/views.py
+++ bookkeeping/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+#INDEX FUNCTION
+def dashboard(request):
+    return render(request, "bookkeeping/dashboard.html")
--- digibooks/settings.py
+++ digibooks/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bookkeeping',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
