b095a88d4ad37b8b691b01852e5c905419b536da
Sthandiwe Msweli <sthandiwemsweli35@gmail.com>
2025-05-20 10:29:40 +0200

    added index.html and modified view.py,urls.py

--- bookkeeping/views.py
+++ bookkeeping/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render,redirect,get_object_or_404
+from django.urls import reverse
 from django.db import IntegrityError
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -9,6 +12,27 @@ from .models import Customer,User
 
 
 # Create your views here.
+#login
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "bookkeeping/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "bookkeeping/login.html")
+    
+
 #register
 def register(request):
     if request.method == "POST":
