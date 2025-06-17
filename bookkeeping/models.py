1c0d04f8e8d50faf707d00b0bda08753c41c6abe
Sthandiwe Msweli <sthandiwemsweli35@gmail.com>
2025-06-17 09:59:54 +0200

    migrated expense model and created expense file

--- bookkeeping/models.py
+++ bookkeeping/models.py
@@ -94,3 +94,13 @@ class ServiceRecord(models.Model):
             return f"{self.customer.name if self.customer else self.customer_name_backup} on {self.service_date}"
         return f"Special - {self.client_name} on {self.service_date}"
 
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="expense")
+    expense = models.CharField(max_length=20, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    expense_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.expense
+
+
